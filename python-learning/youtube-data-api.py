4d27eb31d1b12907ae508acf463afbb2d0b3fc7f
timf34 <farrelti@tcd.ie>
2023-04-30 00:43:57 +0100

    added get_channel_id_from_url method; slight refactor to class

--- python-learning/youtube-data-api.py
+++ python-learning/youtube-data-api.py
@@ -14,13 +14,20 @@ import googleapiclient.discovery
 import googleapiclient.errors
 from google.oauth2.credentials import Credentials
 
-from typing import List, Dict
+from typing import List, Dict, Optional, Tuple
+
+LEX_CHANNEL_ID = "UCSHZKyawb77ixDdsGog4iWA"
+YOUTUBE_DEFAULT_CHANNEL_ID = "UC_x5XG1OV2P6uZZ5FSM9Ttw"
 
 
 class YouTubeDataAPI:
-    def __init__(self, scopes):
-        self.client_secret_file: str = "../client_secret_541571150180-ubpg8s5aeoq9ngefn4tami93c7a0bg37.apps.googleusercontent.com.json"
-        self.scopes: List[str] = ["https://www.googleapis.com/auth/youtube.readonly"]
+    def __init__(
+            self,
+            scopes: Optional[List[str]] = None,
+            client_secret_file: str = "../client_secret_541571150180-ubpg8s5aeoq9ngefn4tami93c7a0bg37.apps.googleusercontent.com.json"
+         ):
+        self.client_secret_file: str = client_secret_file
+        self.scopes: List[str] = scopes or ["https://www.googleapis.com/auth/youtube.readonly"]  # Best practice to use `or` for mutable default values
         self.api_service_name: str = "youtube"
         self.api_version: str = "v3"
         self.credentials = None
@@ -85,15 +92,34 @@ class YouTubeDataAPI:
         return video_ids
 
 
-def print_channel_info():
+    def get_channel_id_from_url(self, url: str) -> str:
+        """Parses a channel url and returns the channel id."""
+        username = url.split("/")[-1]
+
+        # For custom URLs (e.g., "https://www.youtube.com/c/lexfridman")
+        if username.startswith("c/"):
+            username = username[2:]
+        # For URLs with @ symbol (e.g., "https://www.youtube.com/@lexfridman")
+        elif username.startswith("@"):
+            username = username[1:]
+
+        request = self.youtube.channels().list(
+            part="id",
+            forUsername=username
+        )
+        response = request.execute()
+        return response['items'][0]['id'] if len(response['items']) > 0 else None
+
+
+def print_channel_info(channel_id: str = LEX_CHANNEL_ID):
     youtube = YouTubeDataAPI(scopes=["https://www.googleapis.com/auth/youtube.readonly"])
-    response = youtube.get_channel_info("UC_x5XG1OV2P6uZZ5FSM9Ttw")
+    response = youtube.get_channel_info(channel_id)
 
     # Pretty print the json response
     print(json.dumps(response, indent=4, sort_keys=True))
 
 
-def print_video_id():
+def print_video_id(channel_id: str = LEX_CHANNEL_ID):
     youtube = YouTubeDataAPI(scopes=["https://www.googleapis.com/auth/youtube.readonly"])
     video_id = youtube.get_video_id_from_url("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
     print(video_id)
@@ -106,7 +132,21 @@ def print_channel_video_ids():
     print(len(video_ids))
 
 
+def print_video_info(channel_id: str = LEX_CHANNEL_ID):
+    youtube = YouTubeDataAPI(scopes=["https://www.googleapis.com/auth/youtube.readonly"])
+    video_ids = youtube.get_channel_video_ids(channel_id)
+
+    for count, video_id in enumerate(video_ids):
+        response = youtube.get_video_info(video_id)
+        print(json.dumps(response, indent=4, sort_keys=True))
+        if count == 3:
+            break
+
+
 if __name__ == "__main__":
-    print_channel_info()
-    print_video_id()
-    print_channel_video_ids()
+    # print_channel_info()
+    # print_video_id()
+    # print_channel_video_ids()
+    #print_video_info()
+    # youtube = YouTubeDataAPI(scopes=["https://www.googleapis.com/auth/youtube.readonly"])
+    # print(youtube.get_channel_id_from_url("https://www.youtube.com/c/lexfridman"))
