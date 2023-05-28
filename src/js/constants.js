f0b40d345c0b699c89a5ed5a0c8c0e3689dd2cf0
timf34 <farrelti@tcd.ie>
2023-05-28 01:17:52 +0100

    added max results to contants

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,4 +1,4 @@
 // replace with your own YouTube API key
 const API_KEY = "AIzaSyCH3y1_3XlxueC-ecNYfTcHOlTxtOiwL1o";
-const MAX_RESULTS = 15;
+const MAX_RESULTS = 100;
 export { API_KEY, MAX_RESULTS };
