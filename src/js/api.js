b5ad41cd5b1d8eeb660ca400fcf80c4abba73587
timf34 <farrelti@tcd.ie>
2023-12-22 23:12:42 +0000

    gpt4 refactoring of api.js

--- src/js/api.js
+++ src/js/api.js
@@ -1,64 +1,60 @@
-import { API_KEY, MAX_RESULTS} from './constants.js';
+import { API_KEY, MAX_RESULTS } from './constants.js';
 import { getCachedData, cacheData } from './utils.js';
 
-function getChannelIdByUsername(username) {
-
-  console.log("username: ", username);
-  console.log(`https://www.googleapis.com/youtube/v3/channels?key=${API_KEY}&forUsername=${username}&part=id`);
-
-  return fetch(
-      `https://www.googleapis.com/youtube/v3/channels?key=${API_KEY}&forUsername=${username}&part=id`
-  )
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("data: ", data);
-        if (data.items.length > 0) {
-          return data.items[0].id;
-        } else {
-          throw new Error('No channel found with this username');
-        }
-      });
+// Function to handle fetching data from the YouTube API
+async function fetchYouTubeAPI(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`YouTube API responded with status: ${response.status}`);
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("YouTube API request failed:", error);
+    throw error;
+  }
 }
 
+// Function to get channel ID by username
+async function getChannelIdByUsername(username) {
+  const data = await fetchYouTubeAPI(`https://www.googleapis.com/youtube/v3/channels?key=${API_KEY}&forUsername=${username}&part=id`);
+  if (data.items && data.items.length > 0) {
+    return data.items[0].id;
+  } else {
+    throw new Error('No channel found with this username');
+  }
+}
+
+// Function to extract channel ID from URL
 function getChannelId(url) {
   if (url.hostname === 'www.youtube.com' && url.pathname.startsWith('/channel/')) {
     return Promise.resolve(url.pathname.split('/channel/')[1]);
   } else if (url.hostname === 'www.youtube.com' && url.pathname.startsWith('/@')) {
-    let username = url.pathname.split('/@')[1].split('/videos')[0];
+    const username = url.pathname.split('/@')[1].split('/videos')[0];
     return getChannelIdByUsername(username);
   } else {
-    throw new Error('URL is not a YouTube channel URL');
+    throw new Error('URL is not a valid YouTube channel URL');
   }
 }
 
+// Function to get videos of a channel
 async function getVideos(channelId) {
-  // Try to get the videos from the cache
   const cachedVideos = await getCachedData(channelId);
   if (cachedVideos) {
     return cachedVideos;
   }
 
-  const response = await fetch(
-    `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${channelId}&part=snippet,id&order=viewCount&maxResults=${MAX_RESULTS}`
-  );
-  const data = await response.json();
-  console.log("Request successful");
-  const videos = data.items.filter((item) => item.id.kind === "youtube#video");
-  
-  // Cache the videos for future use
-  await cacheData(channelId, videos);
+  const data = await fetchYouTubeAPI(`https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${channelId}&part=snippet,id&order=viewCount&maxResults=${MAX_RESULTS}`);
+  const videos = data.items.filter(item => item.id.kind === "youtube#video");
 
+  await cacheData(channelId, videos);
   return videos;
 }
 
-
-function getVideoStats(videoId) {
-  return fetch(
-    `https://www.googleapis.com/youtube/v3/videos?key=${API_KEY}&id=${videoId}&part=statistics`
-  )
-    .then((response) => response.json())
-    .then((data) => data.items[0].statistics);
+// Function to get video statistics
+async function getVideoStats(videoId) {
+  const data = await fetchYouTubeAPI(`https://www.googleapis.com/youtube/v3/videos?key=${API_KEY}&id=${videoId}&part=statistics`);
+  return data.items[0].statistics;
 }
 
-
-export { getChannelId, getVideos, getVideoStats};
+export { getChannelId, getVideos, getVideoStats };
