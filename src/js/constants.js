3dfb218a7eecf1e764df4d3d01ff75d6edb4b890
timf34 <farrelti@tcd.ie>
2024-01-13 23:14:07 +0000

    updated MAX_RESULTS

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,5 +1,5 @@
 // replace with your own YouTube API key
 const API_KEY = "paste-your-key"; 
-const MAX_RESULTS = 25;
+const MAX_RESULTS = 500;
 const USE_MOCK_DATA = true;
 export { API_KEY, MAX_RESULTS, USE_MOCK_DATA };
