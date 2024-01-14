3acd8de73c392b80d24211f6cb7ca626be929865
timf34 <66926418+timf34@users.noreply.github.com>
2024-01-14 01:01:11 +0000

    Update constants.js

--- src/js/constants.js
+++ src/js/constants.js
@@ -3,5 +3,5 @@ const API_KEY = "your-key";
 const MAX_RESULTS = 500;
 const USE_MOCK_DATA = false;
 const MAX_SEARCH_RESULTS = 5;
-export { API_KEY, MAX_RESULTS, USE_MOCK_DATA };
+export { API_KEY, MAX_RESULTS, USE_MOCK_DATA, MAX_SEARCH_RESULTS };
 
